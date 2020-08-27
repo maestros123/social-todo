ca3fffb34b11ae5f18655b322dee7e2347bcc62e
Stepan <maestro_mag@mail.ru>
2020-08-27 19:40:55 +0300

    Сырой проект

---
+++ js/app.js
@@ -0,0 +1,76 @@
+function createTodoItem(title) {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+
+    const label = document.createElement('label');
+    label.innerText = title;
+    label.className = 'title';
+
+    const editInput = document.createElement('input');
+    editInput.type = 'text';
+    editInput.className = 'textfield';
+
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Изменить';
+    editButton.className = 'edit';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Удалить';
+    deleteButton.className = 'delete';
+
+    const listItem = document.createElement('li');
+    listItem.className = 'todo-item';
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    
+    bindEvents(listItem);
+
+    return listItem;
+}
+
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('.checkbox');
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+    checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
+
+}
+
+function addTodoItem(event) {
+    event.preventDefault();
+
+    if (addInput.value === '') return alert('Задача не введена!');
+
+    const todoItem = createTodoItem(addInput.value);
+    todoList.appendChild(todoItem);
+    addInput.value = '';
+}
+
+function toggleTodoItem() {
+    const listItem = this.parentNode;
+    listItem.classList.toggle('completed');
+}
+
+function editTodoItem() {
+    
+}
+
+function deleteTodoItem() {
+    
+}
+
+const todoForm = document.getElementById('todo-form'),
+    addInput = document.getElementById('add-input'),
+    todoList = document.getElementById('todo-list'),
+    todoItem = document.querySelectorAll('.todo-item');
+
+
+    todoForm.addEventListener('submit', addTodoItem);
